5856a74008fddacbb74141198f416aa8c80684e0
Xiao-xiaochen <200345727+Xiao-xiaochen@users.noreply.github.com>
2026-03-01 22:27:39 +0800

    v0.2.5

--- src/指令集/生产指令/我的科技.ts
+++ src/指令集/生产指令/我的科技.ts
@@ -9,13 +9,13 @@ export function 我的科技(ctx: Context) {
             try {
                 const { userId, username } = await requirePlayer(ctx, session);
                 const 用户资料 = (await ctx.database.get('malieplayer', { userId }))[0]!;
-                const 科技池进度 = Math.floor(用户资料.科技池投入/用户资料.科技池容量)
+                const 科技池进度 = Math.floor((用户资料.科技池投入/用户资料.科技池容量) * 100)
                 return `
 ===[征战文游]===
 ${username} 同志：
 ■ 科技等级：${用户资料.科技等级}
 ■ 科技蓝图：${用户资料.科技蓝图}
-■ 科技池进度：${科技池进度}
+■ 科技池进度：${科技池进度}%
 ■ 科技池容量：${用户资料.科技池容量}
 `.trim();
             } catch (error) {
