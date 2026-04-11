19eb211903b76f46f733d31f95a0ab64737ca0b3
Oppenheymu <oppenheymu@gmail.com>
2026-04-11 20:18:39 +0800

    refactor(类型): 将联军相关类型移动到联军相关目录

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -5,5 +5,5 @@ export * from "./玩家相关/配置类型";
 export * from "./服务类型";
 export * from "./全球数据类型";
 export * from "./地区相关";
-export * from "./联军数据类型";
-export * from "./联军权限类型";
+export * from "./联军相关/联军数据类型";
+export * from "./联军相关/联军权限类型";
