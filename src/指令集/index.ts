897edb7e39040b93bac2dba48fa285308cb8fccf
Oppenheymu <oppenheymu@gmail.com>
2026-05-16 22:34:05 +0800

    style: 改个名

--- src/指令集/index.ts
+++ src/指令集/index.ts
@@ -2,7 +2,7 @@ import type { Context } from 'koishi';
 import { 批量加载插件 } from '@/infrastructure';
 import { 地区指令 } from './地区';
 import { 基础指令 } from './基础';
-import { 生产指令 } from './生产';
+import { 生产指令 } from './玩家核心';
 import { 联军指令 } from './联军';
 
 const 生产插件列表 = [基础指令, 联军指令, 生产指令, 地区指令];
--- src/指令集/玩家核心/index.ts
+++ src/指令集/玩家核心/index.ts
@@ -1,13 +1,13 @@
 import type { Context } from 'koishi';
 import { 批量加载插件 } from '@/infrastructure';
-import { 地堡相关指令 } from './地堡相关';
-import { 工人管理指令 } from './工人管理';
-import { 查询相关指令 } from './查询相关';
+import { 地堡相关指令 } from './地堡';
+import { 工人管理指令 } from './工人';
+import { 查询相关指令 } from './查询';
 import { 注册 } from './注册';
-import { 生产制造指令 } from './生产制造';
-import { 科技相关指令 } from './科技相关';
+import { 生产制造指令 } from './生产';
+import { 科技相关指令 } from './科技';
 import { 签到 } from './签到';
-import { 资源相关指令 } from './资源相关';
+import { 资源相关指令 } from './资源';
 
 const 生产插件列表 = [
     签到,
